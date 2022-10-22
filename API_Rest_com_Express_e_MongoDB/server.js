58c30d009a48626df1cf6459a55d15241bd20012
furtadomn <mnfurtado@yahoo.com.br>
2022-10-22 17:24:51 -0300

    create routes

--- API_Rest_com_Express_e_MongoDB/server.js
+++ API_Rest_com_Express_e_MongoDB/server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const port = 3000;
 
+const routes = {
+  "/": "Curso de Node",
+  "/livros": "Entrei na página de livros",
+  "/autores": "Listagem de autores"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Curso de Node");
+  res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
