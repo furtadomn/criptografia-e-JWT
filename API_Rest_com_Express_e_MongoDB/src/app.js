7b88eabb3d4521111bd6dfef998450f6de5d84a1
furtadomn <mnfurtado@yahoo.com.br>
2022-10-24 20:54:46 -0300

    CRUD

--- API_Rest_com_Express_e_MongoDB/src/app.js
+++ API_Rest_com_Express_e_MongoDB/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const books = [
   {id: 1, "titulo": "Senhor dos Aneis"},
   {id: 2, "titulo": "O Hobbit"},
@@ -14,5 +16,32 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
   res.status(200).json(books);
 });
+
+app.get("/livros/:id", (req, res) => {
+  let index = findBook(req.params.id);
+  res.json(books[index]);
+});
+
+app.post("/livros", (req, res) => {
+  books.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  let index = findBook(req.params.id);
+  books[index].titulo = req.body.titulo;
+  res.json(books);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  let {id} = req.params;
+  let index = findBook(id);
+  books.splice(index, 1);
+  res.send(`Livro ${id} removido com`);
+});
+
+function findBook(id) {
+  return books.findIndex(book => book.id == id);
+};
 
 export default app;
