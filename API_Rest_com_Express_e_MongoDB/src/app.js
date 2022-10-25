0a70e005e33c9e4258316ea1233a58a1c4bc5855
furtadomn <mnfurtado@yahoo.com.br>
2022-10-25 15:05:20 -0300

    create routes to BookController and remove routes from app.js

--- API_Rest_com_Express_e_MongoDB/src/app.js
+++ API_Rest_com_Express_e_MongoDB/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import books from "./models/Book.js"
 import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, "Erro de conexão com o MongoDB"));
@@ -9,36 +8,7 @@ db.once("open", () => {
 });
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
-
-app.get("/livros/:id", (req, res) => {
-  let index = findBook(req.params.id);
-  res.json(books[index]);
-});
-
-app.post("/livros", (req, res) => {
-  books.push(req.body);
-  res.status(201).send("Livro cadastrado com sucesso!");
-});
-
-app.put("/livros/:id", (req, res) => {
-  let index = findBook(req.params.id);
-  books[index].titulo = req.body.titulo;
-  res.json(books);
-});
-
-app.delete("/livros/:id", (req, res) => {
-  let {id} = req.params;
-  let index = findBook(id);
-  books.splice(index, 1);
-  res.send(`Livro ${id} removido com`);
-});
-
-function findBook(id) {
-  return books.findIndex(book => book.id == id);
-};
 
 export default app;
--- API_Rest_com_Express_e_MongoDB/src/routes/booksRoutes.js
+++ API_Rest_com_Express_e_MongoDB/src/routes/booksRoutes.js
@@ -4,6 +4,10 @@ import BookController from "../controllers/booksController.js";
 const router = express.Router();
 
 router
-  .get("/livros", BookController.listBooks);
+  .get("/livros", BookController.listBooks)
+  .get("/livros/:id", BookController.getBook)
+  .post("/livros", BookController.registerBook)
+  .put("/livros/:id", BookController.updateBook)
+  .delete("/livros/:id", BookController.deleteBook)
 
 export default router;
